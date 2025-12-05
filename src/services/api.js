aae125b180c1f42908a21aae6d1156f250908c1b
Leonardo <U22223207@utp.edu.pe>
2025-12-04 23:19:29 -0500

    correción de API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://sigc-backend.onrender.com",
 });
 
 // Interceptor para agregar el token automáticamente a todas las peticiones
